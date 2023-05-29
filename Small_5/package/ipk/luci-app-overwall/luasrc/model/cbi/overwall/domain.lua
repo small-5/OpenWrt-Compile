9fed17f343e13c0f2b68d1629a6a6d471490a31c
small_5 <61473216+small-5@users.noreply.github.com>
2023-05-29 23:00:44 +0800

    Update overwall

--- Small_5/package/ipk/luci-app-overwall/luasrc/model/cbi/overwall/domain.lua
+++ Small_5/package/ipk/luci-app-overwall/luasrc/model/cbi/overwall/domain.lua
@@ -6,6 +6,7 @@ local yb_f=string.format("/etc/overwall/youtube.list")
 local nf_f=string.format("/etc/overwall/netflix.list")
 local cu_f=string.format("/etc/overwall/custom.list")
 local ov_f=string.format("/etc/overwall/oversea.list")
+local ex_f=string.format("/etc/overwall/excluded.list")
 local pre_f=string.format("/etc/overwall/preload.list")
 
 m=Map("overwall",translate("Domain List"))
@@ -120,6 +121,25 @@ o.wrap="off"
 o.cfgvalue=function(self,section) return fs.readfile(ov_f) or "" end
 o.write=function(self,section,value) fs.writefile(ov_f,value:gsub("\r\n","\n")) end
 o.remove=function(self,section,value) fs.writefile(ov_f,"") end
+o.validate=function(self,value)
+    local hosts={}
+    string.gsub(value,'[^'.."\r\n"..']+',function(w) table.insert(hosts,w) end)
+    for index,host in ipairs(hosts) do
+        if not datatypes.hostname(host) then
+            return nil,host.." "..translate("Not valid domain name!")
+        end
+    end
+    return value
+end
+
+s:tab("ex",translate("Excluded Domain List(For Vmess/Vless)"))
+
+o=s:taboption("ex",TextValue,"ex_f","",translate("Excluded Domain List(For Vmess/Vless)"))
+o.rows=15
+o.wrap="off"
+o.cfgvalue=function(self,section) return fs.readfile(ex_f) or "" end
+o.write=function(self,section,value) fs.writefile(ex_f,value:gsub("\r\n","\n")) end
+o.remove=function(self,section,value) fs.writefile(ex_f,"") end
 o.validate=function(self,value)
     local hosts={}
     string.gsub(value,'[^'.."\r\n"..']+',function(w) table.insert(hosts,w) end)
