f9d3f1af4b880c4f4b654c9f6e6c7c6aa7af685b
small_5 <61473216+small-5@users.noreply.github.com>
2023-06-06 20:24:32 +0800

    Remove MTK

--- diy.sh
+++ diy.sh
@@ -7,39 +7,21 @@ case "$OP_TARGET" in
 	R7800)path="R7800";;
 	N1)path="N1";;
 	RPI-4)path="RPI-4";;
-	K2P-16M)path="K2P";A=1;B=1;;
-	K2P-32M)path="K2P";A=1;;
-	MI-AC2100)path="MI-AC2100";A=1;;
-	REDMI-AC2100)path="REDMI-AC2100";A=1;;
-	CR660X)path="CR660X";A=1;;
-	D2)path="D2";A=1;;
-	XY-C5)path="XY-C5";A=1;;
-	R2S)path="R2S";A=2;;
-	R4S)path="R4S";A=2;;
-	R1-PLUS)path="R1-PLUS";A=2;;
-	AC58U)path="AC58U";A=3;;
-	ACRH17)path="ACRH17";A=3;;
-	R619AC-128M)path="R619AC-128M";A=3;;
+	R2S)path="R2S";A=1;;
+	R4S)path="R4S";A=1;;
+	R1-PLUS)path="R1-PLUS";A=1;;
+	AC58U)path="AC58U";A=2;;
+	ACRH17)path="ACRH17";A=2;;
+	R619AC-128M)path="R619AC-128M";A=2;;
 	NANOPI-NEO2)path="NANOPI-NEO2";;
 	*)echo "No adaptation target!";exit 1;;
 esac
 cp -r target/$path/. Small_5
 
 if [ $A = 1 ];then
-	rm -rf openwrt/target/linux/ramips
-	cp -r target/target/kernel-5.4/. Small_5
-	cp -r target/target/ramips/. Small_5
-	chmod 0775 Small_5/target/linux/ramips/mt76x8/base-files/etc/init.d/* Small_5/target/linux/ramips/mt76x8/base-files/lib/upgrade/*
-	chmod 0775 Small_5/target/linux/ramips/mt7620/base-files/etc/init.d/* Small_5/target/linux/ramips/mt7620/base-files/lib/upgrade/*
-	chmod 0775 Small_5/target/linux/ramips/mt7621/base-files/etc/init.d/* Small_5/target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh Small_5/target/linux/ramips/mt7621/base-files/sbin/*
-	chmod 0755 Small_5/target/linux/ramips/rt288x/base-files/lib/upgrade/*
-	chmod 0755 Small_5/target/linux/ramips/rt305x/base-files/lib/upgrade/*
-	chmod 0755 Small_5/target/linux/ramips/rt3883/base-files/lib/upgrade/*
-	[ "$B" = 1 ] && rm -f Small_5/Patch-K2P-32M.patch
-elif [ $A = 2 ];then
 	rm -rf openwrt/package/boot/{uboot-rockchip,arm-trusted-firmware-rockchip} openwrt/package/kernel/linux/modules/video.mk openwrt/target/linux/rockchip
 	cp -r target/target/rockchip/. Small_5
-elif [ $A = 3 ];then
+elif [ $A = 2 ];then
 	rm -rf openwrt/package/firmware/ipq-wifi openwrt/target/linux/ipq40xx
 	cp -r target/target/kernel-5.4/. Small_5
 	cp -r target/target/ipq40xx/. Small_5
