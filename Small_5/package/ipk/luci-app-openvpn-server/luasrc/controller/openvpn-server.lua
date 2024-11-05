c949a3f6d55d69f50e1aa6e0a98506375ab42029
small_5 <61473216+small-5@users.noreply.github.com>
2024-11-05 17:28:18 +0800

    Openvpn: Use log file

--- Small_5/package/ipk/luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
+++ Small_5/package/ipk/luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
@@ -1,4 +1,6 @@
 module("luci.controller.openvpn-server",package.seeall)
+local fs=require"nixio.fs"
+local http=require"luci.http"
 function index()
 	if not nixio.fs.access("/etc/config/openvpn") then
 		return
@@ -11,9 +13,10 @@ function index()
 	entry({"admin","vpn","openvpn-server","client"},form("openvpn-server/client"),_("Client configuration"),2)
 	entry({"admin","vpn","openvpn-server","passwd"},form("openvpn-server/passwd"),_("Username and Password"),3)
 	entry({"admin","vpn","openvpn-server","vars"},form("openvpn-server/vars"),_("Certificate option"),4)
-	entry({"admin","vpn","openvpn-server","log"},template("openvpn-server/log"),_("Log"),5)
+	entry({"admin","vpn","openvpn-server","log"},form("openvpn-server/log"),_("Log"),5)
 	entry({"admin","vpn","openvpn-server","run"},call("act_status"))
-	entry({"admin","vpn","openvpn-server","logdata"},call("act_log"))
+	entry({"admin","vpn","openvpn-server","getlog"},call("getlog"))
+	entry({"admin","vpn","openvpn-server","dellog"},call("dellog"))
 end
 
 function act_status()
@@ -25,9 +28,21 @@ function act_status()
 	luci.http.write_json(e)
 end
 
-function act_log()
-	local log_data={}
-	log_data.syslog=luci.sys.exec("logread | grep 'openvpn(' | sed -e 's/daemon\\S* //' -e 's/([^ ]*\\]//' -e 's/[^ ]* //'")
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(log_data)
+function getlog()
+	logfile="/var/log/openvpn"
+	if not fs.access(logfile) then
+		http.write('')
+		return
+	end
+	local f=io.open(logfile,"r")
+	local a=f:read("*a") or ""
+	f:close()
+	a=string.gsub(a,"\n$","")
+	http.prepare_content("text/plain;charset=utf-8")
+	http.write(a)
+end
+
+function dellog()
+	fs.writefile("/var/log/openvpn","")
+	http.write('')
 end
