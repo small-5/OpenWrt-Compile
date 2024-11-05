3b6e04d8bffd1d1d3f6a98d3d8ad6cf0b2c17d96
small_5 <61473216+small-5@users.noreply.github.com>
2024-11-05 20:28:50 +0800

    Vlmscd: Move log file

--- Small_5/package/ipk/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
+++ Small_5/package/ipk/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
@@ -1,4 +1,6 @@
 module("luci.controller.vlmcsd",package.seeall)
+fs=require"nixio.fs"
+http=require"luci.http"
 function index()
 	if not nixio.fs.access("/etc/config/vlmcsd") then
 		return
@@ -9,7 +11,9 @@ function index()
 	entry({"admin","services","vlmcsd","base"},cbi("vlmcsd/base"),_("Base Setting"),10).leaf=true
 	entry({"admin","services","vlmcsd","config"},form("vlmcsd/config"),_("Config File"),20).leaf=true
 	entry({"admin","services","vlmcsd","log"},form("vlmcsd/log"),_("Log"),30).leaf=true
-	entry({"admin","services","vlmcsd","run"},call("act_status")).leaf=true
+	entry({"admin","services","vlmcsd","run"},call("act_status"))
+	entry({"admin","services","vlmcsd","getlog"},call("getlog"))
+	entry({"admin","services","vlmcsd","dellog"},call("dellog"))
 end
 
 function act_status()
@@ -17,4 +21,23 @@ function act_status()
 	e.running=luci.sys.call("pgrep /usr/bin/vlmcsd >/dev/null")==0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
+end
+
+function getlog()
+	logfile="/var/log/vlmcsd"
+	if not fs.access(logfile) then
+		http.write('')
+		return
+	end
+	local f=io.open(logfile,"r")
+	local a=f:read("*a") or ""
+	f:close()
+	a=string.gsub(a,"\n$","")
+	http.prepare_content("text/plain;charset=utf-8")
+	http.write(a)
+end
+
+function dellog()
+	fs.writefile("/var/log/vlmcsd","")
+	http.write('')
 end
--- Small_5/package/ipk/luci-app-vlmcsd/luasrc/model/cbi/vlmcsd/log.lua
+++ Small_5/package/ipk/luci-app-vlmcsd/luasrc/model/cbi/vlmcsd/log.lua
@@ -1,15 +1,12 @@
-local fs=require "nixio.fs"
-local conffile="/var/vlmcsd.log"
-
+local fs=require"nixio.fs"
+local f,t
 f=SimpleForm("logview")
 f.reset=false
 f.submit=false
 t=f:field(TextValue,"conf")
 t.rmempty=true
 t.rows=20
-function t.cfgvalue()
-	return fs.readfile(conffile) or ""
-end
+t.template="vlmcsd/log"
 t.readonly="readonly"
 
 return f
