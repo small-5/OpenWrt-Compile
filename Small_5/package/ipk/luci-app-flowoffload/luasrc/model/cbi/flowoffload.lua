6f6b54879bad97808a5781392e2a71653fa58abe
small_5 <61473216+small-5@users.noreply.github.com>
2024-11-25 00:36:00 +0800

    Update flowoffload.lua

--- Small_5/package/ipk/luci-app-flowoffload/luasrc/model/cbi/flowoffload.lua
+++ Small_5/package/ipk/luci-app-flowoffload/luasrc/model/cbi/flowoffload.lua
@@ -1,18 +1,11 @@
 m=Map("flowoffload")
 m.title=translate("Turbo ACC Acceleration Settings")
-m.description=translate("Opensource Linux Flow Offload driver (Fast Path or HWNAT)<br/>FULLCONE NAT needs to be set on the firewall page")
+m.description=translate("Opensource Linux Flow Offload driver (Fast Path or HWNAT)")
 m:append(Template("flowoffload/status"))
 
 s=m:section(TypedSection,"flowoffload")
 s.addremove=false
 s.anonymous=true
-
-o=s:option(Flag,"enabled",translate("Enable"))
-o.description=translate("Enable software flow offloading for connections. (decrease cpu load / increase routing throughput)")
-
-o=s:option(Flag,"hw",translate("HWNAT"))
-o.description=translate("Enable Hardware NAT (Requires hardware NAT support)")
-o:depends("enabled",1)
 
 o=s:option(Flag,"bbr",translate("Enable BBR"))
 o.description=translate("Bottleneck Bandwidth and Round-trip propagation time (BBR)")
